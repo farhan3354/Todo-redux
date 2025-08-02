cc42436d6bf0218a7e079378e48d157feae115d5
rahatzafar <rahatzafar4844@gmail.com>
2025-08-02 18:44:37 +0500

    portfolio changes

--- Todo react-redux/src/App.jsx
+++ Todo react-redux/src/App.jsx
@@ -1,50 +1,74 @@
-// import { useCallback, useState } from "react";
-// import { useState } from "react";
-import "./App.css";
-import Child from "./component/child";
-import AddTodo from "./component/Addtodo";
-import { Route, Routes } from "react-router";
-import CounterComponent from "./component/Counter";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { Route, Routes } from "react-router";
+
 import UrlForm from "./component/UrlForm";
+import AddTodo from "./component/Addtodo";
+import CounterComponent from "./component/Counter";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  // const handleclick = useCallback(() => {
-  //   setCount(count + 1);
-  // },[]);
-  // function handleclick() {
-  //   setCount(count + 1);
-  // }
-
   return (
-    <>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/url" element={<UrlForm></UrlForm>}></Route>
-          <Route path="/addtodo" element={<AddTodo></AddTodo>}></Route>
-          {/* <Route path="/" element={<Hero></Hero>}></Route> */}
-          <Route path="/" element={<LandingPage></LandingPage>}></Route>
-          <Route
-            path="/cou"
-            element={<CounterComponent></CounterComponent>}
-          ></Route>
-          {/* <CounterComponent></CounterComponent> */}
-        </Routes>
-      </Provider>
-
-      {/* <div className="card">
-        <button className="text-2xl bg-red-700 border" onClick={handleclick}>
-          count is {count}
-        </button>
-        <br></br>
-        <Child buttonname="Click me" handleclick={handleclick}></Child>
-      </div> */}
-    </>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/url" element={<UrlForm />} />
+        <Route path="/addtodo" element={<AddTodo />} />
+        <Route path="/cou" element={<CounterComponent />} />
+      </Routes>
+    </Provider>
   );
 }
 
 export default App;
+
+// // import { useCallback, useState } from "react";
+// // import { useState } from "react";
+// import "./App.css";
+// import Child from "./component/child";
+// import AddTodo from "./component/Addtodo";
+// import { Route, Routes } from "react-router";
+// import CounterComponent from "./component/Counter";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
+// import UrlForm from "./component/UrlForm";
+// import LandingPage from "./pages/LandingPage";
+
+// function App() {
+//   // const [count, setCount] = useState(0);
+
+//   // const handleclick = useCallback(() => {
+//   //   setCount(count + 1);
+//   // },[]);
+//   // function handleclick() {
+//   //   setCount(count + 1);
+//   // }
+
+//   return (
+//     <>
+//       <Provider store={store}>
+//         <Routes>
+//           <Route path="/url" element={<UrlForm></UrlForm>}></Route>
+//           <Route path="/addtodo" element={<AddTodo></AddTodo>}></Route>
+//           {/* <Route path="/" element={<Hero></Hero>}></Route> */}
+//           <Route path="/" element={<LandingPage></LandingPage>}></Route>
+//           <Route
+//             path="/cou"
+//             element={<CounterComponent></CounterComponent>}
+//           ></Route>
+//           {/* <CounterComponent></CounterComponent> */}
+//         </Routes>
+//       </Provider>
+
+//       {/* <div className="card">
+//         <button className="text-2xl bg-red-700 border" onClick={handleclick}>
+//           count is {count}
+//         </button>
+//         <br></br>
+//         <Child buttonname="Click me" handleclick={handleclick}></Child>
+//       </div> */}
+//     </>
+//   );
+// }
+
+// export default App;
--- Todo react-redux/src/pages/LandingPage.jsx
+++ Todo react-redux/src/pages/LandingPage.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
 import Service from "./Service";
+import Footer from "./Footer";
 
 const LandingPage = () => {
   return (
@@ -77,24 +78,7 @@ const LandingPage = () => {
         </div>
       </section>
 
-      {/* Footer - Updated with your info */}
-      <footer className="bg-gray-800 text-gray-400 py-8">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <p>&copy; {new Date().getFullYear()} Farhan. All rights reserved.</p>
-          <div className="flex justify-center space-x-4 mt-4">
-            {/* Add your social media links */}
-            <a href="#" className="hover:text-white transition-colors">
-              GitHub
-            </a>
-            <a href="#" className="hover:text-white transition-colors">
-              LinkedIn
-            </a>
-            <a href="#" className="hover:text-white transition-colors">
-              Twitter
-            </a>
-          </div>
-        </div>
-      </footer>
+      <Footer></Footer>
     </div>
   );
 };
